1d4fc676543bfcf844a0c4e503b6de7f77fd170e
Rafael Quadros <contatorafaelquadros@gmail.com>
2022-09-05 15:35:36 -0300

    modal color

--- src/scripts/models/toast.js
+++ src/scripts/models/toast.js
@@ -8,7 +8,7 @@ class Toast {
         position: "center", 
         stopOnFocus: true, 
         style: {
-        background: "linear-gradient(to right, #00b09b, #96c93d)",
+        background: "linear-gradient(to right, #4263EB, #4263EB)",
         },
   }).showToast();
     }
